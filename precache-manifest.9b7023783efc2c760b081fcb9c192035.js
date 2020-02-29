0f106e4b73cf2ede7c0cde8dbd59a4d704f34e93
WalternativE <gregor.beyerle@gmail.com>
2020-02-29 19:48:27 +0100

    Updates

---
+++ precache-manifest.9b7023783efc2c760b081fcb9c192035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c163814938d8d504f7def24306b20002",
+    "url": "/monsters-rolodex-ts/index.html"
+  },
+  {
+    "revision": "1e380e14ccf92df75391",
+    "url": "/monsters-rolodex-ts/static/css/main.dda678c4.chunk.css"
+  },
+  {
+    "revision": "22db3f2b5df1c7e77205",
+    "url": "/monsters-rolodex-ts/static/js/2.17690fd0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex-ts/static/js/2.17690fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e380e14ccf92df75391",
+    "url": "/monsters-rolodex-ts/static/js/main.cb317aae.chunk.js"
+  },
+  {
+    "revision": "02a8eea7d38f1e0b4ba7",
+    "url": "/monsters-rolodex-ts/static/js/runtime-main.d6bbaaa0.js"
+  }
+]);
